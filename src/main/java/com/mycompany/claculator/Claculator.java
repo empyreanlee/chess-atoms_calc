7295b9f5d415c89ba5093fd52d39a773ce0de503
Jeremy Gitau <j.leegitau@gmail.com>
2024-06-04 11:06:43 +0300

    Color update

---
+++ src/main/java/com/mycompany/claculator/Claculator.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.claculator;
+
+/**
+ *
+ * @author jleeg
+ */
+public class Claculator{
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
